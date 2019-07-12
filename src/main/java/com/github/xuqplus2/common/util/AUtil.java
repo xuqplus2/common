ec12c7cb8af4235946a3b229d73568c97a398a6e
xuqplus <445172495@qq.com>
2019-07-12 17:37:53 +0800

    init

---
+++ src/main/java/com/github/xuqplus2/common/util/AUtil.java
@@ -0,0 +1,8 @@
+package com.github.xuqplus2.common.util;
+
+public class AUtil {
+
+  public static final void a() {
+    System.out.println(1);
+  }
+}
---
+++ src/test/java/com/github/xuqplus2/common/util/AUtilTest.java
@@ -0,0 +1,11 @@
+package com.github.xuqplus2.common.util;
+
+import org.junit.Test;
+
+public class AUtilTest {
+
+  @Test
+  public void a() {
+    AUtil.a();
+  }
+}
